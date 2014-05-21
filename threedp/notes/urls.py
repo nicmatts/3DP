03c918d39601832731153e9d94ea310210947acb
Nicole M <nicmatts@gmail.com>
2014-05-21 09:06:52 -0500

    make notes editable

--- threedp/notes/urls.py
+++ threedp/notes/urls.py
@@ -6,9 +6,11 @@ urlpatterns = patterns('',
     # ex: /notes/
     url(r'^$', views.index, name='index'),
     # ex: /notes/5/
-    url(r'^(?P<note_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<note_id>\d+)/$', views.note_detail, name='note_detail'),
 
-    url(r'^urgent/$', views.urgent, name='urgent'),
+    url(r'^(?P<note_id>\d+)/edit/$', views.note_edit, name='note_edit'),
+
+    url(r'^urgent/$', views.note_urgent, name='note_urgent'),
     #
-    url(r'^unresolved/$', views.unresolved, name='unresolved'),
+    url(r'^unresolved/$', views.note_unresolved, name='note_unresolved'),
 )
--- threedp/notes/views.py
+++ threedp/notes/views.py
@@ -7,22 +7,34 @@ from django.template import RequestContext
 from django.utils import timezone
 
 from notes.models import Note
+from notes.forms import NoteForm
 # Create your views here.
 
 def index(request):
     all_notes = Note.objects.all()
     return render(request, 'notes/index.html', {'all_notes': all_notes})
 
-def detail(request, note_id):
+def note_detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, 'notes/detail.html', {'note': note})
 
-def urgent(request):
+def note_urgent(request):
 	urgent_notes = Note.objects.filter(urgent=True)
 	context = {'urgent_notes': urgent_notes}
 	return render(request, 'notes/urgent.html', context)
 
-def unresolved(request):
+def note_unresolved(request):
 	unresolved_notes = Note.objects.filter(resolved=False)
 	context = {'unresolved_notes': unresolved_notes}
 	return render(request, 'notes/unresolved.html', context)
+
+def note_edit(request, note_id):
+	note = get_object_or_404(Note, pk=note_id)
+	if request.method=='POST':
+		form = NoteForm(request.POST, instance=note)
+		if form.is_valid():
+			form.save()
+			return redirect('note_detail', note_id)
+	else:
+		form = NoteForm(instance=note)
+	return render_to_response('notes/note_edit.html', {'form': form}, context_instance=RequestContext(request))
