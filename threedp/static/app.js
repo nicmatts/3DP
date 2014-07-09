8ed770d50fc80d8e218bae335b19cd3a7c8d9257
Nicole M <nicmatts@gmail.com>
2014-07-08 20:29:22 -0500

    Calculate job cost

---
+++ threedp/static/app.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+	$("#invoice-submit").click(function() {
+		console.log($( "#id_job_state option:selected" ).text());
+	});
+
+
+    //( "#id_job_state option:selected" ).text();
+    //console.log( $( "#id_job_state option:selected" ).text());
+    //if ($( "#id_job_state option:selected" ).text() == "queued"){console.log("queued");}
+
+	
+
+	$( "#id_actual_time" ).blur(function() {
+		actualTime = $( "#id_actual_time").val();
+		console.log(actualTime);
+		if (actualTime != " "){
+			console.log("Not zero");
+			var newTime = actualTime.split(":");
+			console.log(newTime);
+			var hourCost = (parseInt(newTime[0],10)) * 3;
+			//hourCost = hourCost.toFixed(2);
+			var minuteCost = (parseInt(newTime[1],10)) * 0.05;
+			//minuteCost = minuteCost.toFixed(2);
+			cost = hourCost + minuteCost;
+			console.log(cost);
+			if (cost < 3.00){
+				cost = 3.00;
+			}
+			cost = cost.toFixed(2);
+			console.log(cost);
+			$("#id_job_cost").val(cost);
+		}
+	});
+});
